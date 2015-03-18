3a824bfa761bdbaa61472a33305f977a38284879
christre42 <christian.rennemo@gmail.com>
2015-03-18 15:17:30 +0100

    Stack & Node
    Bruk av Stack som lagring, FIFO(firs in first out). Med tilhørende
    metoder

---
+++ Kap4 - Stack & Node/src/kap4/stakk/Stakk.java
@@ -0,0 +1,12 @@
+
+/**
+ * 18.03.15 - Oppgaven til kap4 i kompendiumet
+ * @Skrevet av Christian
+ */
+
+// En generisk stakk... god forklaring i kompendiumet, not.
+public class Stakk <Type>{
+    public class Node<Type> {
+        
+    }
+}
